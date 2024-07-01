1e8b2740086d5f5021e753476cf296665a01ccf5
Tauan Camargo <lukyhenson@gmail.com>
2024-07-01 20:48:09 -0300

    Initial Commit

---
+++ JokeTestApp/JokeTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeTestAppApp.swift
+//  JokeTestApp
+//
+//  Created by Tauan Tathiell Camargo on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct JokeTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
