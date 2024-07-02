f31efca2e203642f89e758ba47a9d6c98514041c
Tauan Camargo <lukyhenson@gmail.com>
2024-07-01 21:29:57 -0300

    feat: create data service for joke api call

--- JokeTestApp/DataService/DataService.swift
+++ JokeTestApp/DataService/DataService.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct DataService {
+    func getJoke() async -> JokeResponse? {
+        if let url = URL(string: "https://v2.jokeapi.dev/joke/Any?type=single") {
+            let request = URLRequest(url: url)
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(for: request)
+                
+                let decoder = JSONDecoder()
+                
+                do {
+                    let response = try decoder.decode(JokeResponse.self, from: data)
+                    
+                    return response
+                } catch {
+                    print("Error to decode value \(error)")
+                    return nil
+                }
+            } catch {
+                print("Error to fetch api \(error)")
+                return nil
+            }
+        }
+        
+        return nil
+    }
+}
